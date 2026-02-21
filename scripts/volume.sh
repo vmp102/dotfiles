e2b01b57ae395448b25dfebbfb634b6d77f7bf0d
vmp102 <adrianbazan010@gmail.com>
2026-02-21 18:20:17 +0100

    idk

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,10 +1,23 @@
-!/bin/bash
+#!/bin/bash
 
 if [ "$1" == "toggle" ]; then
-  wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+  status=$(playerctl status 2>/dev/null)
+
+  playerctl play-pause
+
+  if [ "$status" == "Playing" ]; then
+    msg="Paused Media"
+  else
+    msg="Resumed Media"
+  fi
+
+  notify-send -h string:x-canonical-private-synchronous:sys-notify \
+    -u low -t 500 "$msg"
+  exit 0
 fi
 
 if [[ "$1" == *%* ]]; then
+  wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
   wpctl set-volume @DEFAULT_AUDIO_SINK@ "$1" --limit 1.0
 fi
 
@@ -13,9 +26,9 @@ is_muted=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -c "\[MUTED\]")
 
 if [ "$is_muted" -eq 1 ]; then
   notify-send -h string:x-canonical-private-synchronous:sys-notify \
-    -u low "Muted"
+    -u low -t 500 "Muted"
 else
   notify-send -h string:x-canonical-private-synchronous:sys-notify \
     -h int:value:"$current_vol" \
-    -u low "Volume: ${current_vol}%"
+    -u low -t 500 "Volume: ${current_vol}%"
 fi
