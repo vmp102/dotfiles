fbef189483e213bb1d96c35c981d72149e17175e
vmp102 <adrianbazan010@gmail.com>
2026-02-26 21:33:57 +0100

    added shadows for floating windows; updated output device switching

--- scripts/audio-switch-device.sh
+++ scripts/audio-switch-device.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-# use pactl list short sinks to view your devices
+# Device IDs
 G435="alsa_output.usb-046d_G435_Wireless_Gaming_Headset_V001008005.1-01.analog-stereo"
 MAJORV="bluez_output.00:25:D1:58:67:6E"
-SPEAKERS="alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Headphones__sink"
 
-case "$1" in
-1) TARGET=$G435 ;;
-2) TARGET=$MAJORV ;;
-3) TARGET=$SPEAKERS ;;
-*) exit 1 ;;
-esac
+CURRENT_SINK=$(pactl get-default-sink)
 
+# Logic for toggling between devices
+if [ "$CURRENT_SINK" = "$G435" ]; then
+  TARGET=$MAJORV
+else
+  TARGET=$G435
+fi
+
+# Check if the target device is actually connected
+if ! pactl list short sinks | grep -q "$TARGET"; then
+  # Target not found; exit
+  exit 0
+fi
+
+# If target found; switch
 pactl set-default-sink "$TARGET"
 
-# Move all currently playing streams to the new sink
+# Move all current audio streams to the new device
 pactl list sink-inputs short | awk '{print $1}' | while read -r stream_id; do
   pactl move-sink-input "$stream_id" "$TARGET"
 done
 
+# Notify using Mako, but only on a successful switch
 FRIENDLY_NAME=$(pactl list sinks | grep -A 20 "$TARGET" | grep "Description:" | cut -d: -f2- | xargs)
-
 notify-send -a "System" -t 2000 "Audio Switched" "Output: $FRIENDLY_NAME"
