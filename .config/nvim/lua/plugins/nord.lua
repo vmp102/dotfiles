8eeddc0f907ed1c6094eef1297d670554242afc1
vmp102 <adrianbazan010@gmail.com>
2026-03-02 16:10:15 +0100

    added keyboard layouts

--- .config/nvim/lua/plugins/nord.lua
+++ .config/nvim/lua/plugins/nord.lua
@@ -1,20 +1,30 @@
 return {
-  -- The actual theme plugin
+  -- 1. Your Nord Theme (Saved)
   {
     "gbprod/nord.nvim",
     lazy = false,
     priority = 1000,
     config = function()
       require("nord").setup({})
-      vim.cmd.colorscheme("nord")
+      -- REMOVED: vim.cmd.colorscheme("nord")
+      -- (We let LazyVim handle the loading instead)
     end,
   },
 
-  -- Tell LazyVim to use it as the default
+  -- 2. Add Gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({})
+    end,
+  },
+
+  -- 3. Tell LazyVim which one to use by default
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nord",
+      colorscheme = "catppuccin-frappe", -- Change this to "nord" to swap back!
     },
   },
 }
